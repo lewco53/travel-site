ecda1b093cc69acd454f554a1f79b5416d0f8345
Dave <whatup@bluespaceinfo.com>
2018-11-29 11:07:05 -0600

    added fade in area lecture 46

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
-var $ = require('jquery');
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu.js';
+import RevealOnScroll from './modules/RevealOnScroll.js';
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "70%");
 
 
 
