375d4b9ea44f67933ff56e78729cd550552e7af7
Dave <whatup@bluespaceinfo.com>
2018-11-29 07:41:12 -0600

    files up to lecture 46

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,40 @@
+var $ = require('jquery');
+import MobileMenu from './modules/MobileMenu.js';
+var mobileMenu = new MobileMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var Person = require('./modules/Person');
+/*import Person from './modules/Person';
+
+class Adult extends Person {
+	payTaxes() {
+		console.log(this.name + " owes so much tax!!");
+	}
+}
+
+var john = new Person("John Doe", "Yellow");
+john.greet();
+
+var jane = new Adult("Jane Smith", "Purple");
+jane.greet();
+jane.payTaxes();
+
+
+$('h1').remove();*/
+
+
