ccb3572bf379060fa733592ee4c84d109bde5b4a
Dave <whatup@bluespaceinfo.com>
2018-11-30 16:06:13 -0600

    next step is lecture 52

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,12 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
+import Modal from './modules/Modal.js';
+
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "70%");
 
